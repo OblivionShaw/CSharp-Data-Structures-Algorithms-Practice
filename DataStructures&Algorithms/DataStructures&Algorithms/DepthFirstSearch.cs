bbeae393a78713e01d6d4c49a4ae586a18c8a3e9
OblivionShaw <sh960208@gmail.com>
2023-12-07 19:09:14 +0800

    feat：深度優先搜尋DepthFirstSearch

---
+++ DataStructures&Algorithms/DataStructures&Algorithms/DepthFirstSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms
+{
+    internal class DepthFirstSearch
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            int R = image.Length;
+            int C = image[0].Length;
+            if (image[sr][sc] == newColor) return image;
+            void dfs(int r, int c, int color)
+            {
+                if (r < 0 || r >= R || c < 0 || c >= C || image[r][c] != color) return;
+                image[r][c] = newColor;
+                dfs(r + 1, c, color);
+                dfs(r - 1, c, color);
+                dfs(r, c + 1, color);
+                dfs(r, c - 1, color);
+            }
+            dfs(sr, sc, image[sr][sc]);
+            return image;
+        }
+    }
+}
--- DataStructures&Algorithms/DataStructures&Algorithms/Form1.cs
+++ DataStructures&Algorithms/DataStructures&Algorithms/Form1.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DataStructures_Algorithms
 {
@@ -14,13 +15,24 @@ namespace DataStructures_Algorithms
             int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
             int target = 9;
             BinarySearch bs = new BinarySearch();
-            MessageBox.Show("二元搜尋Index=" + bs.Search(nums,target));
+            MessageBox.Show("二元搜尋Index=" + bs.Search(nums, target));
             target = 2;
             MessageBox.Show("二元搜尋Index=" + bs.Search(nums, target));
-            nums = new int[] { 3, 5 ,12, 16, 17, 26, 32, 51, 53, 64 };
+            nums = new int[] { 3, 5, 12, 16, 17, 26, 32, 51, 53, 64 };
             target = 32;
             MessageBox.Show("二元搜尋Index=" + bs.Search(nums, target));
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DepthFirstSearch DFS = new DepthFirstSearch();
+            int[][] image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]];
+            int sr = 1, sc = 1, color = 2;
+            int[][] result = new int[][] { };
+            MessageBox.Show("" + image[0][0] + image[0][1] + image[0][2] + "\n\r" + image[1][0] + image[1][1] + image[1][2] + "\n\r" + image[2][0] + image[2][1] + image[2][2] + "\n\r");
+            result = DFS.FloodFill(image, sr, sc, color);
+            MessageBox.Show("" + result[0][0] + result[0][1] + result[0][2] +"\n\r" + result[1][0] + result[1][1] + result[1][2] + "\n\r" + result[2][0] + result[2][1] + result[2][2] + "\n\r");
         }
     }
 }
